307ec600dc3afd6e29a3770ca54d6cb5432eba78
jeflyn <153087335+jeflyn@users.noreply.github.com>
2023-12-17 19:41:29 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,184 @@
+
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>How to Play Congkak</title>
+    <style>
+        
+        body {
+            font-family: Arial, sans-serif;
+            margin: 26px;
+            padding: 0;
+        }
+        h1 {
+            color: #008080;
+            font-size: 40px;
+        }
+        h2 {
+            color: #3E8181;
+            font-size:32px
+        }
+        p {
+            line-height: 1.8;
+            font-size: 22px;
+        }
+        .section-one {
+            background-color: #f0f0f0; /* Light gray background color for section one */
+        }
+        .section-two {
+            background-color: #ffe6cc; /* Light orange background color for section two */
+        }
+        .section-three {
+            background-color: #cce6ff; /* Light blue background color for section three */
+        }
+        .section-four {
+            background-color: #e6ccff; /* Light purple background color for section four */
+        }
+        .section-five {
+            background-color: #ccffe6; /* Light green background color for section five */
+        }
+        .congkak-section {
+            text-align: center; /* Center-align content within this section */
+        }
+        .numbered-option {
+            font-size: 28px;
+            font-weight: bold;
+        }
+        .center{
+            font-size: 22px;
+            text-align:center;
+        }
+        .center-step{
+            font-size: 28px;
+            font-weight: bold;
+            text-align:center;
+        }
+        .underline {
+            text-decoration: underline; /* Add underline to specific words */
+        }
+        .congkak-image {
+            max-width: 70%; /* Ensure the image doesn't exceed its original size */
+            height: auto;
+        }
+        .step1-image {
+            max-width: 50%; /* Ensure the image doesn't exceed its original size */
+            height: auto;
+        }
+        .step2-image {
+            max-width: 50%; /* Ensure the image doesn't exceed its original size */
+            height: auto;
+        }
+        .step3-image {
+            max-width: 50%; /* Ensure the image doesn't exceed its original size */
+            height: auto;
+        }
+        .step4-image {
+            max-width: 50%; /* Ensure the image doesn't exceed its original size */
+            height: auto;
+        }
+        .step5-image {
+            max-width: 50%; /* Ensure the image doesn't exceed its original size */
+            height: auto;
+        }
+        .step6-image {
+            max-width: 50%; /* Ensure the image doesn't exceed its original size */
+            height: auto;
+        }
+        
+    </style>
+</head>
+<body>
+    <header>
+        <h1>How to Play Congkak</h1>
+    </header>
+
+    <main>
+        
+        <section class="congkak-section">
+            <img src="congkak.jpg" alt="Congkak Board" class="congkak-image">
+        </section>
+        
+        <section class="section-one">
+            <h2>Introduction To Congkak</h2>
+            <p>Congkak, also known by various other names across Southeast Asia such as Congklak, Sungka, or Dakon, is a traditional mancala-style game that has roots in various cultures within the religion. It is a two-player game that involves controlling a row of small houses (small hole) and a larger storehouse (large hole) at their respective ends of the board. </p>
+            <p>Congkak requires tactical thinking, planning, and careful execution of moves to outthink the opponent and accumulate more seeds in the storehouse by the end of the game. Congkak is not only a source of entertainment but also holds cultural significance in the regions where it is played, often passed down through generations as a traditional pastime. It fosters social interaction, strategic thinking, and mathematical skills among players.</p>
+            <p>If you’re looking for something fun to play, congkak is a classic Malaysian game that’s easy to learn. While congkak looks and plays similar to mancala, it introduces distinct rules that require consideration before initiating the game. Stay tuned as we will guide you through the gameplay mechanics, including board setup and executing your moves.</p>
+        </section>
+
+        <section class="section-two">
+            <h2>Objective</h2>
+            <p> 1) Accumulate more seeds in your storehouse (large hole) than your opponent.</p>
+            <p> 2) Distribute seeds strategically across the board and capture seeds from your opponent's side.</p>
+            <p> 3) Aim to gather the most seeds into your storehouse by the end of the game.</p>
+            <p> 5) Win by having the highest number of seeds in your storehouse when the game concludes.</p>    
+        </section>
+
+        <section class="section-three">
+            <h2>Game Setup</h2>
+            <p class="numbered-option">Congkak Board </p>
+            <p>The Congkak board consists of two rows of seven holes or cups, with one larger storehouse (big hole) at each end. The holes or cups also known as "lubang" in Malay.</p>
+            <p class="numbered-option">Seeds or Marbles</p>
+            <p>A total of 98 seeds or marbles are used. The seeds should be evenly distributed among the smaller holes on the board, which 7 seeds placed in each of the 14 smaller holes at the start of the game.</p>
+            <p class="numbered-option">Positioning of Players</p>
+            <p>Each player positions themselves in front of their row of houses, with their storehouse located on their left side.</p>
+        </section>
+
+        <section class="section-four">
+            <h2>Gameplay</h2>
+            
+            <p class="center-step">Step 1</p>
+            <section class="congkak-section">
+            <img src="step1.jpg" alt="Step 1" class="step1-image">
+            </section>       
+            <p class="center">Two players position themselves in front of a row of houses, ensuring that their storehouse is situated on their left.</p>
+            <p class="center">Fill each of the houses on the congkak board with 7 pieces each.</p>
+            
+            <p class="center-step">Step 2</p>
+            <section class="congkak-section">
+            <img src="step2.jpg" alt="Step 2" class="step2-image">
+            </section> 
+            <p class="center">When it is on your turn, choose one of the 7 houses in the row closest to you.</p>
+            <p class="center">Take out all the pieces and go clockwise around the  board, dropping 1 piece into each house.</p>
+            <p class="center">Make sure to drop 1 piece into your storehouse when you pass it, but skip over your opponent's storehouse.</p>
+            
+            <p class="center-step">Step 3</p>
+            <section class="congkak-section">
+            <img src="step3.jpg" alt="Step 3" class="step3-image">
+            </section>
+            <p class="center">Continue your turn when you drop your last piece in a house with other pieces</p>
+            <p class="center">If you drop the last piece in your storehouse, choose any other house on your side of the board and take another turn</p>
+            
+            <p class="center-step">Step 4</p>
+            <section class="congkak-section">
+            <img src="step4.jpg" alt="Step 4" class="step4-image">
+            </section>
+            <p class="center">If you drop your final piece into one of your vacant houses, place that piece along with all the pieces in the house directly into your storehouse.</p>
+            <p class="center">If the house on your opponent’s side of the board is empty, place your last piece into your storehouse.</p>
+            
+            <p class="center-step">Step 5</p>
+            <section class="congkak-section">
+            <img src="step5.jpg" alt="Step 5" class="step5-image">
+            </section>
+            <p class="center">You will lose your turn if you put your last piece in one of your opponent's empty house.</p>
+            <p class="center">So, plan wisely your moves to aim for landing in a house containing other pieces.</p>
+            
+            <p class="center-step">Step 6</p>
+            <section class="congkak-section">
+            <img src="step6.jpg" alt="Step 6" class="step6-image">
+            </section>
+            <p class="center">Win if you have more pieces in your storehouse at the last of the game.</p>
+                
+        </section>
+
+        <section class="section-five">
+            <h2>Strategy Tip</h2>
+            <p>Select houses during your turn that enable you to execute multiple consecutive moves. Count the number of pieces within a house before selecting it to determine the final destination of the last piece.</p>
+        </section>
+    </main>
+
+    <footer>
+        <p>&copy; 2023 Congkak Guide</p>
+    </footer>
+</body>
+</html>
+
